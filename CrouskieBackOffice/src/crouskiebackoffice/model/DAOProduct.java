d1fa8d1feef91f83419adab06b80e860684663ab
MENARD-BEDIANT VIRGILE p2100284 <virgile.menard-bediant@etu.univ-lyon1.fr>
2022-10-26 23:02:00 +0200

    DaoProduct almost done

--- CrouskieBackOffice/src/crouskiebackoffice/model/DAOProduct.java
+++ CrouskieBackOffice/src/crouskiebackoffice/model/DAOProduct.java
@@ -1,5 +1,6 @@
 package crouskiebackoffice.model;
 
+import java.math.BigInteger;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -45,5 +46,38 @@ public class DAOProduct extends DAO {
     public Boolean setPriceOf(int idProduct, Float newPrice) throws SQLException {
         Object[] args = {newPrice, idProduct};
         return super.execute("UPDATE " + tableName + " SET priceprod = ? WHERE idprod = ?", args) == 1;
+    }
+
+    public Boolean exist(Product product) throws SQLException {
+        Object[] args = {product.getId()};
+        return ((int) super.selectAll("SELECT count(idprod) FROM " + tableName + " WHERE idprod = ?", args).get(0)[0]) == 1;
+    }
+
+    public int getIdOf(Product product) throws SQLException {
+        int id = -1;
+        Object[] args = {product.getName(), product.getDescription(), product.getPrice()};
+        List<Object[]> liste = super.selectAll("SELECT idprod FROM " + tableName + " WHERE nameprod = ? and descriptionprod = ? and priceprod = ?", args);
+        if (!liste.isEmpty()) {
+            id = ((int) liste.get(0)[0]);
+        }
+        return id;
+    }
+
+    private int getLastIdInserted() throws SQLException {
+        return ((BigInteger) super.selectAll("SELECT LAST_INSERT_ID(IDPROD) from PRODUCT order by LAST_INSERT_ID(IDPROD) desc limit 1;", null).get(0)[0]).intValue();
+    }
+
+    public Boolean insertOrUpdate(Product product) throws SQLException {
+        int idExistant = product.getId();
+        if (idExistant != -1 || (idExistant = getIdOf(product)) != -1) {
+            product.setId(idExistant);
+            Object[] args = {product.getName(), product.getDescription(), product.getPrice(), product.getId()};
+            return super.execute("UPDATE " + tableName + " SET nameprod = ?, descriptionprod = ?, priceprod = ? WHERE idprod = ?", args) == 1;
+        } else {
+            Object[] args = {product.getName(), product.getDescription(), product.getPrice()};
+            Boolean res = super.execute("INSERT INTO " + tableName + " (nameprod, descriptionprod, priceprod) VALUES (?, ?, ?)", args) == 1;
+            product.setId(getLastIdInserted());
+            return res;
+        }
     }
 }

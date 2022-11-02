3150a1c842f9cbba572c5e36190c56cedf70c7e1
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-02 12:15:59 +0100

    fix css link

--- views/v_header.php
+++ views/v_header.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="Language" content="<?= LANG ?>"/>
 		<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0"/>
-		<link rel="stylesheet" href="<?= PATH_CSS ?> main.css">
+		<link rel="stylesheet" href="<?= PATH_CSS ?>main.css">
 	</head> 
 	<body>
 		<!-- En-tête -->

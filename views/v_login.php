d1e5e0f90dbc7313bb29fd624090e78ae2a78d7b
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-03 11:54:36 +0100

    add logiun page

--- views/v_login.php
+++ views/v_login.php
@@ -5,20 +5,22 @@ require_once(PATH_VIEWS . 'head.php');
 <!--  Début de la page -->
 <img src="<?=PATH_LOGOS . "crouskie-geant.png"?>" alt="crouskie géant" class="background">
 <div class="flex column content">
-    <div>
+    <div class="logo">
         <a href="index.php">
             <!-- Il faut trouver une solution pour le changement de couleur dynamique -->
             <img src=<?= PATH_LOGOS . "crouskie-text-outlined-red.svg" ?> height="120px" width="414px" alt="<?= LOGO ?>">
         </a>
     </div>
-    <div class="flex column">
+    <div class="flex column center instruction">
         <!-- Enregistrer les textes dans les langs -->
         <h3>Connexion / Inscription</h3>
-        <p>Saisissez votre e-mail pour vous<b>connecter ou créer un compte</p>
+        <p>Saisissez votre e-mail pour vous<br>connecter ou créer un compte</p>
     </div>
-    <form class="card flex column shadow rounded">
-        <input type="email" name="email" id="email" placeholder="E-mail" class="icon rounded shadow" data-icon="<?= PATH_ICONS . "email-outline.svg"?>">
+    <form class="card flex column center shadow rounded">
+        <input type="email" name="email" id="email" placeholder="E-mail" class="icon rounded shadow-small" data-icon="<?= PATH_ICONS . "email-outline.svg"?>">
+        <input type="submit" value="Connexion / Inscription" class="valid circle">
     </form>
+    <a href="#" class="underlined">Gérer mes crouskies</a>
 </div>
 
 <!--  Fin de la page -->

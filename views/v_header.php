48fa6306b78a0a0b97a971fbb21b07d715cc739a
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-03 09:42:49 +0100

    add dyn css

--- views/v_header.php
+++ views/v_header.php
@@ -7,6 +7,14 @@
 		<meta name="Language" content="<?= LANG ?>"/>
 		<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0"/>
 		<link rel="stylesheet" href="<?= PATH_CSS ?>main.css">
+
+		<!-- Ajout css dynamique -->
+		<?php
+		if (isset($page) && is_file(PATH_CSS.$page."css")) {
+			echo("<link rel=\"stylesheet\" href=\"".PATH_CSS.$page.".css\">");
+		}
+		?>
+		
 	</head> 
 	<body>
 		<!-- En-tête -->

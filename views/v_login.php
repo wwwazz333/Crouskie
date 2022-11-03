cc989b698a5fc0821b661c375c0bf5399603a024
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-03 11:31:18 +0100

    update main.css with shadow box and v_login

--- views/v_login.php
+++ views/v_login.php
@@ -3,14 +3,22 @@
 require_once(PATH_VIEWS . 'head.php');
 ?>
 <!--  Début de la page -->
-
-<div class="flex column">
+<img src="<?=PATH_LOGOS . "crouskie-geant.png"?>" alt="crouskie géant" class="background">
+<div class="flex column content">
     <div>
         <a href="index.php">
             <!-- Il faut trouver une solution pour le changement de couleur dynamique -->
             <img src=<?= PATH_LOGOS . "crouskie-text-outlined-red.svg" ?> height="120px" width="414px" alt="<?= LOGO ?>">
         </a>
     </div>
+    <div class="flex column">
+        <!-- Enregistrer les textes dans les langs -->
+        <h3>Connexion / Inscription</h3>
+        <p>Saisissez votre e-mail pour vous<b>connecter ou créer un compte</p>
+    </div>
+    <form class="card flex column shadow rounded">
+        <input type="email" name="email" id="email" placeholder="E-mail" class="icon rounded shadow" data-icon="<?= PATH_ICONS . "email-outline.svg"?>">
+    </form>
 </div>
 
 <!--  Fin de la page -->

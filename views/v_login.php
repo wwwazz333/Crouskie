53ad88a96ab8e3c477ff6c2d92b6e8f6a8f8f0e9
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-03 11:16:14 +0100

    add box component

--- views/v_login.php
+++ views/v_login.php
@@ -8,7 +8,7 @@ require_once(PATH_VIEWS . 'head.php');
     <div>
         <a href="index.php">
             <!-- Il faut trouver une solution pour le changement de couleur dynamique -->
-            <img src=<?= PATH_LOGOS . "crouskie-text-filled-white.svg" ?> height="50px" width="200px" alt="<?= LOGO ?>">
+            <img src=<?= PATH_LOGOS . "crouskie-text-outlined-red.svg" ?> height="120px" width="414px" alt="<?= LOGO ?>">
         </a>
     </div>
 </div>

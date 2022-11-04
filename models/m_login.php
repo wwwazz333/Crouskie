3001fc0847806effb1aaf769e7fb0fc0888825f9
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-04 12:01:34 +0100

    update login model

--- models/m_login.php
+++ models/m_login.php
@@ -3,10 +3,6 @@ require_once(PATH_MODELS . 'UtilisateurDAO.php');
 
 function checkMail(string $email) : bool
 {
-    try {
-        $DAO = new UtilisateurDAO(DEBUG);
-        return $DAO->isEmailExist($email);
-    } catch (Exception $ex) {
-        return false;
-    }
+    $DAO = new UtilisateurDAO(DEBUG);
+    return $DAO->isEmailExist($email);
 }

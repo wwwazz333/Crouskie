eeebcb8c8f27524fa304510b5f229ba2e6ea3264
MENARD-BEDIANT VIRGILE p2100284 <virgile.menard-bediant@etu.univ-lyon1.fr>
2022-11-04 11:06:26 +0100

    starting creation main window

--- CrouskieBackOffice/src/crouskiebackoffice/CrouskieBackOffice.java
+++ CrouskieBackOffice/src/crouskiebackoffice/CrouskieBackOffice.java
@@ -1,14 +1,8 @@
 package crouskiebackoffice;
 
-import crouskiebackoffice.model.ClothSize;
-import crouskiebackoffice.model.Color;
 import crouskiebackoffice.model.ConnectionDB;
 import crouskiebackoffice.model.DAOProductBought;
-import crouskiebackoffice.model.Product;
-import crouskiebackoffice.model.ProductBought;
-import crouskiebackoffice.model.ProductColorSize;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 
 public class CrouskieBackOffice {
 
@@ -18,13 +12,13 @@ public class CrouskieBackOffice {
                 System.out.println(pr);
             }
 
-            new DAOProductBought().insertOrUpdate(
-                    new ProductBought(
-                            new ProductColorSize(
-                                    new Product(1, "", "", 0f), new Color("rouge"), new ClothSize(2, "S"), 55),
-                            1,
-                            new Timestamp(0))
-            );
+//            new DAOProductBought().insertOrUpdate(
+//                    new ProductBought(
+//                            new ProductColorSize(
+//                                    new Product(1, "", "", 0f), new Color("rouge"), new ClothSize(2, "S"), 55),
+//                            1,
+//                            new Timestamp(0))
+//            );
             ConnectionDB.getInstance().close();
 
         } catch (NumberFormatException | SQLException e) {

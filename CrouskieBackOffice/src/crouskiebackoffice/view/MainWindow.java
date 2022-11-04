c8d49bdaa19a2722c9f5178d2a9a324ae6457431
MENARD-BEDIANT VIRGILE p2100284 <virgile.menard-bediant@etu.univ-lyon1.fr>
2022-11-04 12:16:31 +0100

    setup disposition and banner

--- CrouskieBackOffice/src/crouskiebackoffice/view/MainWindow.java
+++ CrouskieBackOffice/src/crouskiebackoffice/view/MainWindow.java
@@ -18,66 +18,67 @@ public class MainWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        headerPane = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
+        headerPanel1 = new crouskiebackoffice.view.HeaderPanel();
         mainPane = new javax.swing.JPanel();
         tabPane = new javax.swing.JTabbedPane();
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        statusbarPane = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        visualisationPanel1 = new crouskiebackoffice.view.VisualisationPanel();
+        addingPanel1 = new crouskiebackoffice.view.AddingPanel();
+        editionPanel1 = new crouskiebackoffice.view.EditionPanel();
+        statusbarPanel1 = new crouskiebackoffice.view.StatusbarPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Crouskie BackOffice");
         setBackground(new java.awt.Color(255, 255, 255));
         setLocationByPlatform(true);
         setPreferredSize(new java.awt.Dimension(720, 480));
-
-        headerPane.setLayout(new javax.swing.BoxLayout(headerPane, javax.swing.BoxLayout.LINE_AXIS));
-
-        jLabel2.setText("Crouskie");
-        headerPane.add(jLabel2);
-
-        getContentPane().add(headerPane, java.awt.BorderLayout.NORTH);
+        getContentPane().setLayout(new java.awt.BorderLayout());
+        getContentPane().add(headerPanel1, java.awt.BorderLayout.NORTH);
 
         mainPane.setLayout(new java.awt.CardLayout());
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout visualisationPanel1Layout = new javax.swing.GroupLayout(visualisationPanel1);
+        visualisationPanel1.setLayout(visualisationPanel1Layout);
+        visualisationPanel1Layout.setHorizontalGroup(
+            visualisationPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 400, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 229, Short.MAX_VALUE)
+        visualisationPanel1Layout.setVerticalGroup(
+            visualisationPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 221, Short.MAX_VALUE)
         );
 
-        tabPane.addTab("tab1", jPanel1);
+        tabPane.addTab("Visualiser", visualisationPanel1);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout addingPanel1Layout = new javax.swing.GroupLayout(addingPanel1);
+        addingPanel1.setLayout(addingPanel1Layout);
+        addingPanel1Layout.setHorizontalGroup(
+            addingPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 400, Short.MAX_VALUE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 229, Short.MAX_VALUE)
+        addingPanel1Layout.setVerticalGroup(
+            addingPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 221, Short.MAX_VALUE)
         );
 
-        tabPane.addTab("tab2", jPanel2);
+        tabPane.addTab("Ajouter", addingPanel1);
 
-        mainPane.add(tabPane, "card2");
+        javax.swing.GroupLayout editionPanel1Layout = new javax.swing.GroupLayout(editionPanel1);
+        editionPanel1.setLayout(editionPanel1Layout);
+        editionPanel1Layout.setHorizontalGroup(
+            editionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        editionPanel1Layout.setVerticalGroup(
+            editionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 221, Short.MAX_VALUE)
+        );
+
+        tabPane.addTab("Éditer", editionPanel1);
+
+        mainPane.add(tabPane, "tabPane");
 
         getContentPane().add(mainPane, java.awt.BorderLayout.CENTER);
-
-        statusbarPane.setLayout(new javax.swing.BoxLayout(statusbarPane, javax.swing.BoxLayout.LINE_AXIS));
-
-        jLabel1.setText("Test...");
-        statusbarPane.add(jLabel1);
-
-        getContentPane().add(statusbarPane, java.awt.BorderLayout.SOUTH);
+        getContentPane().add(statusbarPanel1, java.awt.BorderLayout.SOUTH);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -116,13 +117,12 @@ public class MainWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel headerPane;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private crouskiebackoffice.view.AddingPanel addingPanel1;
+    private crouskiebackoffice.view.EditionPanel editionPanel1;
+    private crouskiebackoffice.view.HeaderPanel headerPanel1;
     private javax.swing.JPanel mainPane;
-    private javax.swing.JPanel statusbarPane;
+    private crouskiebackoffice.view.StatusbarPanel statusbarPanel1;
     private javax.swing.JTabbedPane tabPane;
+    private crouskiebackoffice.view.VisualisationPanel visualisationPanel1;
     // End of variables declaration//GEN-END:variables
 }

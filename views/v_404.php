31d9b0fab1c4836bcfa320f485b14995c32180ea
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-04 11:11:13 +0100

    fix page 404

--- views/v_404.php
+++ views/v_404.php
@@ -1,3 +1,10 @@
-<?php require_once(PATH_VIEWS . 'header.php'); ?>
-<h1>Page non trouvée</h1>
-<?php require_once(PATH_VIEWS . 'footer.php'); ?>
+<?php require_once(PATH_VIEWS . 'head.php'); ?>
+<div class="flex column center content">
+    <div class="logo">
+        <a href="index.php">
+            <!-- Il faut trouver une solution pour le changement de couleur dynamique -->
+            <img src=<?= PATH_LOGOS . "crouskie-text-outlined-red.svg" ?> height="120px" width="414px" alt="<?= LOGO ?>">
+        </a>
+    </div>
+    <h1>Malheusement, cette page n'existe pas...</h1>
+</div>

abcafcce67443766c8c4d799ebf9165241e582ba
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-07 10:24:01 +0100

    add product entitie

---
+++ entities/Product.php
@@ -0,0 +1,46 @@
+<?php
+// représente un produit vendu par crouskie
+class Product{
+    // properties
+    private int $_id;
+    private int $_collection_id;
+    private string $_name;
+    private string $_description;
+    private float $_price;
+
+    function __construct__(int $id, int $collection_id, string $name, string $description, float $price)
+    {
+        $this->_id = $id;
+        $this->_collection_id;
+        $this->_name = $name;
+        $this->_description = $description;
+        $this->_price = $price;
+    }
+
+    // Getters and setters
+
+    public function getId() : int
+    {
+        return $this->_id;
+    }
+
+    public function getCollectionId() : int
+    {
+        return $this->_collection_id;
+    }
+
+    public function getName() : string
+    {
+        return $this->_name;
+    }
+
+    public function getDescription() : string
+    {
+        return $this->_description;
+    }
+
+    public function getPrice() : float
+    {
+        return $this->_price;
+    }
+}

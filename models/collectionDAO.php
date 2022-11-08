7059ab977a5fe6eff2378e48581d1ddbe63d45f0
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-08 15:07:00 +0100

    ajout get collections by id

--- models/collectionDAO.php
+++ models/collectionDAO.php
@@ -1,16 +1,25 @@
 <?php
-    require_once(PATH_MODELS . 'DAO.php');
+require_once(PATH_MODELS . 'DAO.php');
 
-    class collectionDAO extends DAO{
+class collectionDAO extends DAO
+{
 
-        // Obtenir tout les produits associé à un nom
-    public function getCollectionsByName(string $name){
-        $result = $this->queryAll("SELECT * FROM collection WHERE NAMECOLLECTION LIKE ?",array('%' . $name . '%'));
+    // Obtenir tout les produits associé à un nom
+    public function getCollectionsByName(string $name)
+    {
+        $result = $this->queryAll("SELECT * FROM collection WHERE NAMECOLLECTION LIKE ?", array('%' . $name . '%'));
         return $this->resultToProductsArray($result);
     }
-        //get all the existing collections
-        public function getCollections() : array{
-            $result = $this->queryAll("SELECT * FROM collection");
-            return $this->resultToProductsArray($result);
-        }
+    //get all the existing collections
+    public function getCollections(): array
+    {
+        $result = $this->queryAll("SELECT * FROM collection");
+        return $this->resultToProductsArray($result);
     }
+    // Obtenir un produit par son ID
+    public function getProductByID(int $id)
+    {
+        $product = $this->queryRow("SELECT * FROM product WHERE idprod = ?", array($id));
+        return $product == false ? false : $product;
+    }
+}

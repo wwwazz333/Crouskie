1e80ec431405dc67082315a8c5d872b79b10b98d
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-08 15:27:24 +0100

    modification collection class add getter

--- entities/Collection.php
+++ entities/Collection.php
@@ -1,13 +1,14 @@
 <?php
 // représente un produit vendu par crouskie
-class Collection{
+class Collection
+{
     // properties
     private string $_name;
     private int $_collection_id;
     private string $_path_picture;
-    
-    
-    
+
+
+
     function __construct__(int $name, int $_collection_id, string $_path_picture)
     {
         $this->_name = $name;
@@ -16,6 +17,16 @@ class Collection{
     }
 
     // Getters and setters
-
-    
+    function getName(): string
+    {
+        return $this->_name;
+    }
+    function getCollectionId(): int
+    {
+        return $this->_collection_id;
+    }
+    function getPathPicture(): string
+    {
+        return $this->_path_picture;
+    }
 }

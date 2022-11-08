bad5d757d97a339f9716d18303d0d9ffedc9a805
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-08 15:11:24 +0100

    add  features result to collection array

--- models/collectionDAO.php
+++ models/collectionDAO.php
@@ -4,22 +4,36 @@ require_once(PATH_MODELS . 'DAO.php');
 class collectionDAO extends DAO
 {
 
+    public function resultToCollectionArray(array $result){
+        $collection = [];
+        foreach($result as $collection){
+            array_push($collection, 
+                new Product(
+                    $collection['NAMECOLLECTION'],
+                    $collection['IDCOLLECTION'],
+                    $collection['PATHPICTURE']
+                )
+            );
+        }
+        return $collection;
+    }
+    
     // Obtenir tout les produits associé à un nom
     public function getCollectionsByName(string $name)
     {
         $result = $this->queryAll("SELECT * FROM collection WHERE NAMECOLLECTION LIKE ?", array('%' . $name . '%'));
-        return $this->resultToProductsArray($result);
+        return $this->resultToCollectionArray($result);
     }
     //get all the existing collections
     public function getCollections(): array
     {
         $result = $this->queryAll("SELECT * FROM collection");
-        return $this->resultToProductsArray($result);
+        return $this->resultToCollectionArray($result);
     }
     // Obtenir un produit par son ID
     public function getProductByID(int $id)
     {
-        $product = $this->queryRow("SELECT * FROM product WHERE idprod = ?", array($id));
-        return $product == false ? false : $product;
+        $collection = $this->queryRow("SELECT * FROM coll$collection WHERE idprod = ?", array($id));
+        return $collection == false ? false : $collection;
     }
 }

a081a519f2ffe229f5256e6f7156335221c72413
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-10 19:13:35 +0100

    avancement dans la page acceuil

--- views/v_accueil.php
+++ views/v_accueil.php
@@ -6,10 +6,14 @@ require_once(PATH_VIEWS . 'header.php');
 <!--  Début de la page -->
 <div class="hero">
     <!-- alt dynamique à faire -->
-    <img src="<?=PATH_IMAGES . 'backgrounds/background.jpg' ?>" alt="background">
+    <img src="<?= PATH_IMAGES . 'backgrounds/background.jpg' ?>" alt="background">
+
+    <p>Découvrez la nouvelle collection Crousk-hiver</p>
+    <button class='rounded'>Découvrir</button>
+
 </div>
 <!--  Fin de la page -->
 
 
 <!--  Pied de page -->
-<?php require_once(PATH_VIEWS.'footer.php')?>
+<?php require_once(PATH_VIEWS . 'footer.php') ?>

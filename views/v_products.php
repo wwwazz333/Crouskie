f7406cf3c7346f63ab722e290749c059bae04bcc
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-10 22:33:56 +0100

    changement du css

--- views/v_products.php
+++ views/v_products.php
@@ -8,19 +8,16 @@ require_once(PATH_VIEWS . 'header.php');
     <input type="text" name="search" id="input-search" class="rounded shadow-small" placeholder="Rechercher...">
 </div>
 <div class="flex row wrap products">
-    <div class='flex column rounded'>
+    <div class='flex column rounded' style='height:fit-content;'>
         <img class='rounded' src="<?= PATH_IMAGES . 'backgrounds/background.jpg' ?>" alt="background">
         <div class='flex row' style='padding-top:10px;background-color:transparent;'>
             <div class='flex column'>
                 <p>Crouskie Hoodie</p>
                 <p>45€</p>
             </div>
-            <iconify-icon icon="akar-icons:shopping-bag"></iconify-icon>
+            <iconify-icon class='circle'  icon="akar-icons:shopping-bag"></iconify-icon>
         </div>
     </div>
-
-
-
 </div>
 <!--  Fin de la page -->
 

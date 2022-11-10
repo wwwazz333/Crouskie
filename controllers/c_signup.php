521b119f18c190ece21163cbb251f6244df97bbe
BERIOT MARC p2100238 <marc.beriot@etu.univ-lyon1.fr>
2022-11-10 21:30:57 +0100

    debut check mot de passe

--- controllers/c_signup.php
+++ controllers/c_signup.php
@@ -1,4 +1,17 @@
 <?php
 
+// On regarde si il s'agit d'une tentative de connexion
+if (isset($_POST['email'])) {
+    
+}
 
-require_once(PATH_VIEWS . $page . '.php');
+
+// On vérifie que l'email est bien présente dans l'URL avant d'afficher la page
+if (isset($_GET['email'])) {
+    // Dans ce cas on affiche la page
+    require_once(PATH_VIEWS . $page . '.php');
+}else{
+    // Dans le cas contraire on le redirige vers la page portal pour lui redemander son email
+    header('Location: index.php?page=portal');
+    exit();
+}

f6a8f51dcbcbbf537d6b4977fc752b4d488cfbb8
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-11 19:36:13 +0100

    implémentation du product dao pour récup les product et les afficher

--- views/v_products.php
+++ views/v_products.php
@@ -8,16 +8,23 @@ require_once(PATH_VIEWS . 'header.php');
     <input type="text" name="search" id="input-search" class="rounded shadow-small" placeholder="Rechercher...">
 </div>
 <div class="flex row wrap products">
-    <div class='flex column rounded' style='height:fit-content;'>
+    <?php 
+    $productsDao=(new ProductDAO(DEBUG));
+    $resultProducts=$productsDao->getProducts();
+    $productArray=$productsDao->resultToProductsArray($resultProducts);
+    foreach ($productArray as $product) {?>
+        <div class='flex column rounded' style='height:fit-content;'>
         <img class='rounded' src="<?= PATH_IMAGES . 'backgrounds/background.jpg' ?>" alt="background">
         <div class='flex row' style='padding-top:10px;background-color:transparent;'>
             <div class='flex column'>
-                <p>Crouskie Hoodie</p>
-                <p>45€</p>
+                <p><?=$product->getName()?></p>
+                <p><?=$product->getPrice()?></p>
             </div>
-            <iconify-icon class='circle'  icon="akar-icons:shopping-bag"></iconify-icon>
+            <iconify-icon class='circle'icon="akar-icons:shopping-bag"></iconify-icon>
         </div>
-    </div>
+    </div>  
+    <?php } ?>
+    
 </div>
 <!--  Fin de la page -->
 

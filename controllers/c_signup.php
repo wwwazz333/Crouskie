00beea6617d730d2557458a032e7053c11f458de
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-11-11 17:46:53 +0100

    update signup page and first implementation of signup

--- controllers/c_signup.php
+++ controllers/c_signup.php
@@ -2,16 +2,23 @@
 
 // On regarde si il s'agit d'une tentative de connexion
 if (isset($_POST['email'])) {
-    
+    require_once(PATH_MODELS . 'UtilisateurDAO.php');
+    $DAO = new UtilisateurDAO(DEBUG);
+    $res = $DAO->addUser($_POST['email'],
+        password_hash($_POST['password'],PASSWORD_DEFAULT),
+        $_POST['firstname'],
+        $_POST['lastname']
+    );
+    header('Location: index.php');
+    exit();
 }
 
-
 // On vérifie que l'email est bien présente dans l'URL avant d'afficher la page
-if (isset($_GET['email'])) {
-    // Dans ce cas on affiche la page
-    require_once(PATH_VIEWS . $page . '.php');
-}else{
-    // Dans le cas contraire on le redirige vers la page portal pour lui redemander son email
+if (isset($_GET['email']) == false) {
+    // Dans le cas ou elle n'existe pas on redirige vers le portail de connexion
     header('Location: index.php?page=portal');
     exit();
 }
+
+// Dans le cas contraire on affiche la vue
+require_once(PATH_VIEWS . $page . '.php');

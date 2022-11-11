72b44e66c10261abfcd38dd31bf38a6871df20a6
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-11 19:41:08 +0100

    ajout des l'affichage de la liste des products dans la products

--- controllers/c_products.php
+++ controllers/c_products.php
@@ -1,2 +1,6 @@
 <?php
+require_once(PATH_MODELS . 'ProductDAO.php');
+$DAO = new ProductDAO(DEBUG);
+$products = $DAO->resultToProductsArray($DAO->getProducts());
+
 require_once(PATH_VIEWS . $page . '.php');
--- views/v_products.php
+++ views/v_products.php
@@ -2,17 +2,14 @@
 //  En tête de page
 require_once(PATH_VIEWS . 'head.php');
 require_once(PATH_VIEWS . 'header.php');
+
 ?>
 <!--  Début de la page -->
 <div class="flex row center search">
     <input type="text" name="search" id="input-search" class="rounded shadow-small" placeholder="Rechercher...">
 </div>
 <div class="flex row wrap products">
-    <?php 
-    $productsDao=(new ProductDAO(DEBUG));
-    $resultProducts=$productsDao->getProducts();
-    $productArray=$productsDao->resultToProductsArray($resultProducts);
-    foreach ($productArray as $product) {?>
+    <?php foreach ($productArray as $product) {?>
         <div class='flex column rounded' style='height:fit-content;'>
         <img class='rounded' src="<?= PATH_IMAGES . 'backgrounds/background.jpg' ?>" alt="background">
         <div class='flex row' style='padding-top:10px;background-color:transparent;'>
@@ -24,7 +21,6 @@ require_once(PATH_VIEWS . 'header.php');
         </div>
     </div>  
     <?php } ?>
-    
 </div>
 <!--  Fin de la page -->
 

340d345bfcade646c4c68e82c3e68261babae402
BERIOT MARC p2100238 <marc.beriot@etu.univ-lyon1.fr>
2022-11-11 22:12:09 +0100

    création pages account

---
+++ views/v_account.php
@@ -0,0 +1,14 @@
+<?php
+//  En tête de page
+require_once(PATH_VIEWS . 'head.php');
+require_once(PATH_VIEWS . 'header.php');
+?>
+<!--  Début de la page -->
+
+
+
+<!--  Fin de la page -->
+
+
+<!--  Pied de page -->
+<?php require_once(PATH_VIEWS . 'footer.php') ?>

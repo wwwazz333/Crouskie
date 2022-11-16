3de73f7e41f6d1ba6b9cc39359d33f20831077a5
BERIOT MARC p2100238 <marc.beriot@etu.univ-lyon1.fr>
2022-11-16 21:39:37 +0100

    debut forme informations

--- views/v_account_info.php
+++ views/v_account_info.php
@@ -1,15 +1,12 @@
 <div class="flex column wrap right info">
-    <h2>Test aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h2>
-    <div id="identity" class="flex row">
+    <div id="identity" class="flex row rounded">
         <h2>Nom:</h2>
         <h2>Prénom:</h2>
     </div>
-    <div class="">
+    <div class="rounded">
         <h2>Adresse mail:</h2>
     </div>
-    <div class="">
-        <a>
-            <h2>Changer son mot de passe</h2>
-        </a>
-    </div>
+    <a href="index.php" class ="rounded">
+        <h2>Changer son mot de passe</h2>
+    </a>
 </div>

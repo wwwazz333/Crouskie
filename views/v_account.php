b61014571ac8d1f2106acb81817ed77324b46175
anne passelegue <anne.passelegue@etu.univ-lyon1.fr>
2022-11-16 17:42:28 +0100

    Alignements page account

--- views/v_account.php
+++ views/v_account.php
@@ -6,16 +6,19 @@ require_once(PATH_VIEWS . 'header.php');
 
 <!-- affichage menu -->
 
-<div class="flex column wrap right" id="menu">
-    <a href="index.php?page=account&selected=info"> <p>Information du compte</p> </a>
-    <a href="index.php?page=account"> <p>Mes commandes</p> </a>
-    <a href="index.php?page=account"> <p>Mes favoris</p> </a>
+<div class="flex row">
+    <div class="flex column wrap right" id="menu">
+        <a href="index.php?page=account&selected=info"> <p>Information du compte</p> </a>
+        <a href="index.php?page=account"> <p>Mes commandes</p> </a>
+        <a href="index.php?page=account"> <p>Mes favoris</p> </a>
+    </div>
+
+
+    <!-- require_once view $selectedpage -->
+    <?php
+    require_once(PATH_VIEWS . $page . "_" . $selectedPage . '.php');
+    ?>
 </div>
 
-
-<!-- require_once view $selectedpage -->
-<?php
-require_once(PATH_VIEWS . $page . "_" . $selectedPage . '.php');
-?>
 <!--  Pied de page -->
 <?php require_once(PATH_VIEWS . 'footer.php') ?>

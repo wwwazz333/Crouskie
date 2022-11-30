1362169a1cc149dd8eafd0e4a0ee1363855c43e5
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-30 15:29:17 +0100

    implémentation de l'image de background de l'acceuil qui est désormais celle de la dernière collection

--- views/v_accueil.php
+++ views/v_accueil.php
@@ -6,9 +6,8 @@ require_once(PATH_VIEWS . 'header.php');
 <!--  Début de la page -->
 <div class="hero">
     <!-- alt dynamique à faire -->
-    <img src="<?= PATH_IMAGES . 'backgrounds/background.jpg' ?>" alt="background">
+    <img src="<?= $lastCollection->getPathPicture() ?>" alt="background">
     <div class='flex column'>
-        <?php print_r($lastCollection); ?>
         <p><?=COLLECTION_DISCOVER?></p>
         <p>Crousk-hiver</p>
         <button class='rounded'><?=DISCOVER?></button>

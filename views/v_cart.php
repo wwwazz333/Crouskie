3fadc6f39e59339b26bbe471e03b2d5ce23f0f7e
BERIOT MARC p2100238 <marc.beriot@etu.univ-lyon1.fr>
2022-12-06 17:02:56 +0100

    ajout page cart

---
+++ views/v_cart.php
@@ -0,0 +1,11 @@
+<?php
+//  En tête de page
+require_once(PATH_VIEWS . 'head.php');
+require_once(PATH_VIEWS . 'header.php');
+?>
+
+
+
+
+<!--  Pied de page -->
+<?php require_once(PATH_VIEWS . 'footer.php') ?>

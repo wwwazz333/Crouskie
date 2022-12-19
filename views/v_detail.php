dad5a89b554ae74733e453ddb43c88fa7f8e6e5f
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2022-12-20 00:48:06 +0100

    begin detail page

--- views/v_detail.php
+++ views/v_detail.php
@@ -4,3 +4,26 @@ require_once(PATH_VIEWS . 'head.php');
 require_once(PATH_VIEWS . 'header.php');
 
 ?>
+
+
+<div class="flex row content">
+    <div class="carousel">
+
+    </div>
+    <div class="flex column properties card">
+        <h2>Pull crouskie Edition Scam</h2>
+        <p>Sweatshirt</p>
+        <div class="colors-container">
+            <p>Couleurs</p>
+            <div class="colors">
+                
+            </div>
+        </div>
+        <div class="flex row">
+            <h2 id="price">
+                45€
+            </h2>
+            <button class="rounded" type="submit">Ajouter au panier</button>
+        </div>
+    </div>
+</div>

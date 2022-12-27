ff72f2277cb2d169c380c2fc2585eb80ef4405e7
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-12-27 19:37:50 +0100

    ajout a la vue v_collection

--- views/v_collections.php
+++ views/v_collections.php
@@ -7,7 +7,7 @@ require_once(PATH_VIEWS . 'header.php');
 <!--  Début de la page -->
 <div class="flex column center" id="collections">
     <?php foreach ($collections as $collection) { ?>
-        <a href="index.php?page=collection_produit&id=<?= $collection->getCollectionId() ?>">
+        <a href="index.php?page=collection_product&id=<?= $collection->getCollectionId() ?>">
             <div class="collection-content">
                 <img class="rounded" src="<?= $collection->getPathPicture() ?>" alt="background">
                 <p><?= $collection->getName() ?></p>

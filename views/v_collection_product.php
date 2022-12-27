e9bef07fcd9dcd6adf14464b5368f961fdbcc81d
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-12-27 19:37:32 +0100

    ajout des de la gestion des collection dans la vue

---
+++ views/v_collection_product.php
@@ -0,0 +1,33 @@
+<?php
+//  En tête de page
+require_once(PATH_VIEWS . 'head.php');
+require_once(PATH_VIEWS . 'header.php');
+
+?>
+<!--  Début de la page -->
+
+<h1><?=  $collection->getName()?></h1>
+<div class="flex row wrap center" id="products">
+    <?php foreach ($products as $product) { ?>
+        <a href="index.php?page=detail&id=<?= $product->getId() ?>">
+            <div class='flex column rounded shadow-small product-item'>
+                <div class="image-container">
+                    <img class='rounded shadow-small' src="<?= PATH_IMAGES . 'backgrounds/background.jpg' ?>" alt="background">
+                    <div class="circle icon-button">
+                        <iconify-icon icon="mdi:cards-heart-outline"></iconify-icon>
+                    </div>
+                </div>
+
+                <div class='flex column center details'>
+                    <p><?= $product->getName() ?></p>
+                    <p><?= $product->getPrice() ?>€</p>
+                </div>
+            </div>
+        </a>
+    <?php } ?>
+</div>
+<!--  Fin de la page -->
+
+
+<!--  Pied de page -->
+<?php require_once(PATH_VIEWS . 'footer.php') ?>

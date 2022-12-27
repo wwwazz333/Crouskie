40fc78e3093130612908553045b622c43452d725
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-12-27 19:36:29 +0100

    ajout du controleur

---
+++ controllers/c_collection_product.php
@@ -0,0 +1,18 @@
+<?php
+require_once(PATH_MODELS . 'ProductDAO.php');
+require_once(PATH_MODELS . 'CollectionDAO.php');
+
+$DAO = new ProductDAO(DEBUG);
+$DAOCollection = new CollectionDAO(DEBUG);
+if (isset($_GET['id'])) {
+    
+    $products = $DAO->resultToProductsArray($DAO->getProductsByCollectionId($_GET['id']));
+    $collection = $DAOCollection->getCollectionsByID($_GET['id']);
+    $collection = new Collection( //create an object from the DAO
+        $collection['namecollection'],
+        $collection['idcollection'],
+        $collection['pathpicture']
+    );
+}
+
+require_once(PATH_VIEWS . $page . '.php');

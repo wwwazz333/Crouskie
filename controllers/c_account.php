8d90ad5e282ba0b88b68ac0b96cb1fc7fb00a2e5
anne passelegue <anne.passelegue@etu.univ-lyon1.fr>
2022-12-29 22:12:50 +0100

    Gestion du cas où user n'a aucune commande

--- controllers/c_account.php
+++ controllers/c_account.php
@@ -17,25 +17,32 @@ $userId = $user->getIdUser();
 $commandes = $commandeDAO->getCommandeByCustomerId($userId);
 
 
-// On récupère le nom des produits de chaque commande à partir de l'id de la commande
-// Pour ensuite pouvoir les afficher dans la vue
-foreach ($commandes as $commande) { 
-    $id = $commande['numorder'];
-    //$cmd = $commande->getCommandeByID($id);
-    
-    //print_r($cmd);
+// Si l'utilisateur n'a encore passé aucune commande
+if($commandes == null) {
+    $isCommandesEmpty = true;
+// Sinon
+} else {
+    $isCommandesEmpty = false;
 
-    // Récupération des information de chaque commande
-    list($date, $heure,) = explode(' ', $commande['dateorder']); 
-    $listeCommande[$id] = [
-        "date" => $date,
-        "heure" => $heure,
-        "numorder" => $commande['numorder']
-    ];
+    // On récupère les infos sur les commandes et
+    // On récupère les infors sur les produits àcheté de chaque commande à partir de l'id de la commande
+    // Pour ensuite pouvoir les afficher dans la vue
+    foreach ($commandes as $commande) { 
+        $id = $commande['numorder'];
+        //$cmd = $commande->getCommandeByID($id);
+        
+        //print_r($cmd);
+
+        // Récupération des information de chaque commande
+        list($date, $heure,) = explode(' ', $commande['dateorder']); 
+        $listeCommande[$id] = [
+            "date" => $date,
+            "heure" => $heure,
+            "numorder" => $commande['numorder']
+        ];
+    }
 }
 
 
 // Vue
 require_once(PATH_VIEWS . $page . '.php');
-
-print_r($listeCommande);
--- views/v_account_commande.php
+++ views/v_account_commande.php
@@ -1,5 +1,5 @@
-<div class="commandes shadow rounded">
-    
+<div class="commandes shadow rounded" id="commandes-pas-vide">
+
     <!-- Affichage des commandes -->
     <?php foreach ($listeCommande as $c) { ?>
         <details>
@@ -8,5 +8,26 @@
         </details>
 
     <?php } ?>
-
 </div>
+
+<div class="ccommandes shadow rounded" id="commandes-vide">
+    <h1><?= COMMANDES_VIDE ?></h1>
+</div>
+
+<!-- Affichage selon si le panier est vide ou non -->
+<script>
+        // transforme la variable isCommandesEmpty PHP en variable page JavaScript
+        var estVide = <?php echo json_encode($isCommandesEmpty); ?>;
+        const divCommandes = document.getElementById("commandes-pas-vide")
+        const divCommandesVide = document.getElementById("commandes-vide")
+        // gestion de l'affichage de la bonne div
+        if (estVide) {
+            divCommandes.style.display = "none"
+            divCommandesVide.style.display = "block"
+        } else {
+            divCommandes.style.display = "block"
+            divCommandesVide.style.display = "none"
+        }
+</script>
+
+

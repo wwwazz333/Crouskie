d7dbe8e9d349cca7b2ef5271c918970362ed518f
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2023-01-03 21:29:53 +0100

    add PopUp message support

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -28,3 +28,94 @@ function getJson(url, callback) {
     })
     .catch((error) => { callback({ success : false }); });
 }
+
+// Méthodes pour activer ou désactiver la capacité à naviger dans une page WEB
+
+function disableScroll() {
+  // On récupère la position courante
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  // On redéfinit l'évènement onscroll pour qu'elle reste figée
+  window.onscroll = function() {
+      window.scrollTo(scrollLeft, scrollTop);
+  };
+}
+
+function enableScroll() {
+  window.onscroll = function() {};
+}
+
+// Méthode PopUp pour afficher une alerte à l'utilisateur
+
+var activePopUp = null
+
+/**
+ * 
+ * @param {*} type Définit le type de la PopUp (Information (0),Réussite (1), Avertissement (2), Erreur (3))
+ * @param {*} title Définit le titre de la PopUp
+ * @param {*} message Définit le message de la PopUp
+ */
+function PopUp(type,title,message) {
+  if (activePopUp) {
+    activePopUp.remove();
+  }
+
+  const container = document.createElement('div');
+  container.id = 'PopUp';
+  container.classList.add('flex','center');
+
+  const card = document.createElement('div');
+  card.classList.add('rounded','flex','column');
+  container.appendChild(card);
+
+  const icon_container = document.createElement('div');
+  icon_container.classList.add('fill-w','flex','center');
+  card.appendChild(icon_container);
+
+  const titleElement = document.createElement('h2');
+  titleElement.innerText = title;
+  card.appendChild(titleElement);
+
+  const messageElement = document.createElement('p');
+  messageElement.innerText = message;
+  card.appendChild(messageElement);
+
+  const closeButton = document.createElement('a');
+  closeButton.innerText = 'Fermer';
+  closeButton.classList.add('circle');
+  closeButton.addEventListener('click',()=>{
+    activePopUp.remove();
+    enableScroll();
+    activePopUp = null;
+  });
+  card.appendChild(closeButton);
+
+  switch (type) {
+    case 0:
+      icon_container.style = 'background-color: #03a9f4';
+      closeButton.style = 'background-color: #03a9f4';
+      icon_container.innerHTML = '<iconify-icon icon="mdi:information-outline" style="color: white"></iconify-icon>';
+      break;
+    case 1:
+      icon_container.style = 'background-color: #82E105';
+      closeButton.style = 'background-color: #82E105';
+      icon_container.innerHTML = '<iconify-icon icon="mdi:success-circle-outline" style="color: white"></iconify-icon>';
+      break;
+    case 2:
+      icon_container.style = 'background-color: #ffb74d';
+      closeButton.style = 'background-color: #ffb74d';
+      icon_container.innerHTML = '<iconify-icon icon="material-symbols:warning-outline-rounded" style="color: white"></iconify-icon>';
+      break;
+    case 3:
+      icon_container.style = 'background-color: #E10512';
+      closeButton.style = 'background-color: #E10512';
+      icon_container.innerHTML = '<iconify-icon icon="material-symbols:error-outline-rounded" style="color: white"></iconify-icon>';
+      break;
+    default:
+      break;
+  }
+
+  document.body.appendChild(container);
+  activePopUp = container;
+  disableScroll();
+}

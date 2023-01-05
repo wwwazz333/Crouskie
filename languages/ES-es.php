3529f686e1405cb2c0f50e173e98cc4fb8a443ae
Elliot Casna p2103912 <elliot.casna@etu.univ-lyon1.fr>
2023-01-05 22:32:39 +0100

    add ES lang

---
+++ languages/ES-es.php
@@ -0,0 +1,74 @@
+<?php
+define('TITRE', 'Crouskie');
+// Imágenes
+define('LOGO', 'Logo de Crouskie'); // Mostrado si no se encuentra la imagen
+define('SHOPPINGBAG','Logo de la bolsa de compras');
+define('CROUKIE_GEANT','Crouskie gigante en el fondo');
+
+// Inicio
+define('COLLECTION_DISCOVER','Descubre la nueva colección');
+define('DISCOVER','Descubrir');
+
+// Menús
+define('MENU_COLLECTIONS','Colecciones');
+define('MENU_PRODUITS','Productos');
+
+// Pie de página
+define('RESEAUX_SOCIAUX','Encuéntranos en:');
+
+// Mensajes de error generales
+define('TEXTE_PAGE_404','¡Oops, la página solicitada no existe!');
+define('MESSAGE_ERREUR',"Se ha producido un error");
+define('ERREUR_QUERY', 'Problema al acceder a la base de datos. Contacte al administrador');
+define('SUCCESS','Éxito');
+define('ERROR','Error');
+define('INFORMATION','Información');
+define('WARNING','Advertencia');
+
+// Otros
+define('CLOSE','Cerrar');
+
+// Iniciar sesión
+define('CONNEXION','Iniciar sesión');
+define('DISCONNECTION','Cerrar sesión');
+define('CONNECTED','Ahora estás conectado');
+define('DISCONNECTED','Ahora estás desconectado');
+define('MON_COMPTE','Mi cuenta');
+
+// Página de inicio de sesión
+define('INSCRIPTION','Registrarse');
+define('CONNEXION_INSCRIPTION','Iniciar sesión / Registrarse');
+define('INSTRUCTIONS_PORTAL','Ingresa tu correo electrónico para iniciar sesión o crear una cuenta');
+define('INSTRUCTIONS_SIGNUP','Ingresa tus datos para crear una cuenta');
+define('INSTRUCTIONS_SIGNIN','Ingresa tu contraseña para iniciar sesión');
+define('PASSWORD','Contraseña');
+define('PASSWORD_CONFIRM','Confirmar contraseña');
+define('CREATE_PASSWORD','Crear una contraseña');
+define('LASTNAME','Apellido');
+define('FIRSTNAME','Nombre');
+define('GERER_COOKIES','Administrar mis croukies');
+define('MAUVAIS_MDP','¡Contraseña incorrecta!');
+define('FAIL_CREATE_ACCOUNT','¡Se produjo un error al crear tu cuenta!');
+
+// Página de cuenta
+define('CHANGER_INFORMATIONS','Modificar mis datos');
+define('ENREGISTRER_INFORMATIONS','Guardar mis datos');
+define('CHANGER_MDP','Cambiar mi contraseña');
+define('COMMANDES_VIDE','¡Aún no tienes ningún pedido!');
+
+// Página del carrito
+define('MONTANT_TOTAL','Monto total del carrito: ');
+define('PASSER_COMMANDE','Realizar mi pedido');
+define('VIDER_PANIER','Vaciar mi carrito');
+define('PANIER_VIDE','¡Tu carrito está vacío!');
+define('PANIER_PAS_CONNECTE','¡No estás conectado!');
+
+// Página de productos
+define('RECHERCHER','Buscar...');
+
+// Página de detalles
+define('COULEUR','Color:');
+define('TAILLE','Talla:');
+define('AJOUTER_PANIER','Añadir al carrito');
+define('AJOUT_PANIER_SUCCES','Añadido al carrito con éxito');
+define('AJOUT_PANIER_FAIL',"¡Ocurrió un error al agregar al carrito!");

0ddbaa8b2bace3c4eb5d65c29f0a01069965fa4a
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2023-01-06 15:23:39 +0100

    update requiere

--- views/v_account_info.php
+++ views/v_account_info.php
@@ -1,34 +1,33 @@
 <div class="flex column">
 
-    <div class="flex row wrap right contenu">
+    <form action="index.php?page=account" method="post" class="flex row wrap right contenu">
 
-        <form action="index.php?page=account" method="post" class="card flex row center shadow-small rounded">
+        <div class="card flex row center shadow-small rounded">
             <h2>Nom :</h2>
-            <input type="text" name="nom" id="nom" class="icon rounded shadow-small" disabled="disabled" value="<?= $user->getLastName()?>">
-        </form>
+            <input type="text" name="nom" id="nom" class="icon rounded shadow-small" disabled="disabled" value="<?= $user->getLastName() ?>">
+        </div>
 
-        <form action="index.php?page=account" method="post" class="card flex row center shadow-small rounded">
+        <div class="card flex row center shadow-small rounded">
             <h2>Prénom :</h2>
-            <input type="text" name="prenom" id="prenom" class="icon rounded shadow-small" disabled="disabled" value="<?= $user->getFirstName()?>">
-        </form>
+            <input type="text" name="prenom" id="prenom" class="icon rounded shadow-small" disabled="disabled" value="<?= $user->getFirstName() ?>">
+        </div>
 
-        <form action="index.php?page=account" method="post" class="card flex row center shadow-small rounded">
+        <div class="card flex row center shadow-small rounded">
             <h2>Adresse mail :</h2>
-            <input type="email" name="email" id="email" class="icon rounded shadow-small" disabled="disabled" value="<?= $user->getEmail()?>">
-        </form>
-    </div>
-
-    <form class="flex row wrap right contenu center">
-        <input type="button" id="form-info" value="<?= CHANGER_INFORMATIONS ?>" class="valid circle">
-        <input type="button" id="form-enregistrer" value="<?= ENREGISTRER_INFORMATIONS ?>" class="valid circle">
-        <a href = "index.php?page=changepwd" id="mdp">
-            <input type="button" id="form-mdp" value="<?= CHANGER_MDP ?>" class="valid circle">
-        </a>
+            <input type="email" name="email" id="email" class="icon rounded shadow-small" disabled="disabled" value="<?= $user->getEmail() ?>">
+        </div>
+        <div class="flex row wrap right contenu center">
+            <input type="button" id="form-info" value="<?= CHANGER_INFORMATIONS ?>" class="valid circle">
+            <input type="submit" id="form-enregistrer" value="<?= ENREGISTRER_INFORMATIONS ?>" class="valid circle">
+            <a href="index.php?page=changepwd" id="mdp">
+                <input type="button" id="form-mdp" value="<?= CHANGER_MDP ?>" class="valid circle">
+            </a>
+        </div>
     </form>
 
     <?php
-		if (isset($page) && is_file(PATH_SCRIPTS.$page.".js")) {
-			echo("<script src=\"".PATH_SCRIPTS.$page."_".$selectedPage.".js\"></script>");
-		}
-	?>
+    if (isset($page) && is_file(PATH_SCRIPTS . $page . ".js")) {
+        echo ("<script src=\"" . PATH_SCRIPTS . $page . "_" . $selectedPage . ".js\"></script>");
+    }
+    ?>
 </div>

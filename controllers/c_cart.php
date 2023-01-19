61d3407303228d8803c6a0a63a95a54d1bbe5e5a
BERIOT MARC p2100238 <marc.beriot@etu.univ-lyon1.fr>
2023-01-19 15:28:23 +0100

    transition page cart -> valider commande

--- controllers/c_cart.php
+++ controllers/c_cart.php
@@ -1,18 +1,12 @@
 <?php 
 require_once(PATH_MODELS . 'CartDAO.php');
-require_once(PATH_MODELS . 'CommandeDAO.php');
 require_once(PATH_MODELS . 'ProductDAO.php');
-require_once(PATH_MODELS . 'ProductBoughtDAO.php');
-require_once(PATH_MODELS . 'StockDAO.php');
 require_once(PATH_MODELS . 'SizeDAO.php');
 require_once(PATH_ENTITY . 'User.php');
 
 // Si l'utilisateur est connecté
 // Récupération du panier de l'utilisateur
 if($isLogged) {
-    $commandeDAO = new CommandeDAO(DEBUG);
-    $productBoughtDAO = new ProductBoughtDAO(DEBUG);
-    $stockDAO = new StockDAO(DEBUG);
     $cartDAO = new CartDAO(DEBUG);
     $userId = $user->getIdUser();
     $cartPhp = $cartDAO->getCartByCustomerId($userId); // Récupération du panier de l'utilisateur -> type : objet php
@@ -113,61 +107,7 @@ if($isLogged) {
                 // On valide la commande
                 case 'valider':
                     
-                    $enoughQuantity = true;
-                    $defaultProd;
-                    
-                    //On regarde si les produits sont toujours en stock
-                    foreach($cart as $productCart){
-                        if(!$stockDAO->isProductInStockWithQuantity( $productCart->getIdProd(), $productCart->getColorCart(), $productCart->getSizeCart(), $productCart->getQuantityCart())){
-                            $enoughQuantity = false;
-                            $defaultProd = $productCart;
-                        }
-                    }
-
-
-                    if($enoughQuantity){
-                        // Création de la commande
-                        $resultOrder = $commandeDAO->addCommande($userId);
-                        // On récupère l'idOrder pour plus tard 
-                        $idOrder = $commandeDAO->getNumLastCommande($userId)[0][0];
-                        $resultProducts = true;
-
-                        foreach ($cart as $productCart) {
-                            $color = $productCart->getColorCart();
-                            $size = $productCart->getSizeCart();
-                            $idProd = $productCart->getIdProd();
-                            $quantity = $productCart->getQuantityCart();
-                            $idCustomer = $productCart->getCustomerId();
-
-                            $result = $productBoughtDAO->buyProduct($color, $idProd, $size, $idOrder, $quantity, $idCustomer);
-                            // Si un seul produit ne peux pas être acheter alors false
-                            $resultProducts = $resultProducts && $result;
-                        }
-                    }
-
-                    if(!$enoughQuantity){
-                        $textToDisplay = $product->getName() . ' ' . $defaultProd->getColorCart()  . ' '  . PAS_ASSEZ_DE_STOCK;
-                        $product = $productDAO->resultToProduct($productDAO->getProductByID($id));
-                        $alert = showAlert(3, QUANTITE_INSUFFISANTE, $textToDisplay);
-
-                    }
-                    else{
-                        // Alertes pour savoir si la fonction marche
-                        if($resultProducts && $resultOrder){
-                            header('Location: index.php?page=cart&valider=1');
-                        }
-
-                        else if($resultProducts && !$resultOrder){
-                            $alert = showAlert(3, COMMANDE, "Commande");
-                        }
-                        else if(!$resultProducts && $resultOrder){
-                            $alert = showAlert(3, COMMANDE, "Result");
-                        }
-                        else{
-                            $alert = showAlert(3, COMMANDE, "$");
-                        }
-                    }
-                    break;
+                    header('Location: index.php?page=valider_commande');
             }
 
         }
